8f2e03ab9bec4c649e281c46bab592749188eac1
George Bespamiatnykh <george.bespamiatnykh@gmail.com>
2026-02-17 21:42:08 +0700

    feat: add url_checks table

--- database.sql
+++ database.sql
@@ -1,7 +1,18 @@
 DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
 
 CREATE TABLE urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) UNIQUE NOT NULL,
+    created_at DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE url_checks (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls(id) NOT NULL,
+    status_code INT,
+    h1 varchar(255),
+    title varchar(255),
+    description varchar(255),
     created_at DATE DEFAULT CURRENT_DATE
 );
