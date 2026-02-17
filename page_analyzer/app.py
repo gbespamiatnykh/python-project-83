ad9598a4ddd6f3fd2edb1b7cd5a318d3bf2c7327
George Bespamiatnykh <george.bespamiatnykh@gmail.com>
2026-02-17 21:45:06 +0700

    feat: update app.py and url_repository.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -20,31 +20,40 @@ def index():
 
 
 @app.route("/urls", methods=["POST"])
-def post_url():
+def add_url():
     url_data = request.form.get("url")
     errors = validate(url_data)
     if errors:
         flash("Некорректный URL", "danger")
         return redirect(url_for("index"))
 
-    new_url = normalize_url(url_data)
-    url = repo.get_by_url(new_url)
+    normalized_url = normalize_url(url_data)
+    url = repo.find_by_name(normalized_url)
     if url:
         flash("Страница уже существует", "info")
         return redirect(url_for("show_url", id=url.get("id")))
     creation_date = date.today()
-    id = repo.save(new_url, creation_date)
+    id = repo.add(normalized_url, creation_date)
     flash("Страница успешно добавлена", "success")
     return redirect(url_for("show_url", id=id))
 
 
 @app.route("/urls/<int:id>")
 def show_url(id):
-    url = repo.get_by_id(id)
-    return render_template("show.html", url=url)
+    url = repo.find_by_id(id)
+    checks = repo.get_checks(id)
+    return render_template("show.html", url=url, checks=checks)
 
 
 @app.route("/urls")
 def list_urls():
     urls = repo.get_all()
     return render_template("list.html", urls=urls)
+
+
+@app.route("/urls/<int:id>/checks", methods=["POST"])
+def add_check(id):
+    check_date = date.today()
+    repo.add_check(id, check_date)
+    flash("Страница успешно проверена", "success")
+    return redirect(url_for("show_url", id=id))
--- page_analyzer/url_repository.py
+++ page_analyzer/url_repository.py
@@ -7,7 +7,6 @@ class UrlRepository:
         self.db_url = db_url
 
     def _execute(self, query, params=None, fetch=None, do_commit=False):
-        result = None
         with psycopg2.connect(self.db_url) as conn:
             with conn.cursor(cursor_factory=RealDictCursor) as cur:
                 cur.execute(query, params)
@@ -19,28 +18,62 @@ class UrlRepository:
                 conn.commit()
         return result
 
-    def save(self, url, date):
+    def add(self, url, date):
         row = self._execute(
             """
             INSERT INTO urls (name, created_at) VALUES (%s, %s)
             RETURNING id
             """,
             (url, date),
-            "one",
-            True,
+            fetch="one",
+            do_commit=True,
         )
         return row["id"]
 
-    def get_by_url(self, url):
+    def find_by_name(self, url):
         return self._execute(
-            "SELECT * FROM urls WHERE name = %s", (url,), "one"
+            "SELECT * FROM urls WHERE name = %s", (url,), fetch="one"
         )
 
-    def get_by_id(self, id):
-        return self._execute("SELECT * FROM urls WHERE id = %s", (id,), "one")
+    def find_by_id(self, id):
+        return self._execute(
+            "SELECT * FROM urls WHERE id = %s", (id,), fetch="one"
+        )
 
     def get_all(self):
         return self._execute(
-            "SELECT * FROM urls ORDER BY id DESC",
-            fetch="all"
-            )
+            """
+            SELECT u.id AS id, u.name AS name, MAX(uc.created_at) AS last_check
+            FROM urls u LEFT JOIN url_checks uc ON u.id = uc.url_id
+            GROUP BY u.id, u.name ORDER BY u.id DESC;
+            """,
+            fetch="all",
+        )
+
+    def add_check(self, url_id, date):
+        return self._execute(
+            """
+            INSERT INTO url_checks (url_id, created_at) VALUES (%s, %s)
+            RETURNING id
+            """,
+            (url_id, date),
+            fetch="one",
+            do_commit=True,
+        )
+
+    def get_checks(self, id):
+        return self._execute(
+            "SELECT * FROM url_checks WHERE url_id = %s ORDER BY id DESC",
+            (id,),
+            fetch="all",
+        )
+
+    def get_last_check(self, id):
+        return self._execute(
+            """
+            SELECT * FROM url_checks WHERE url_id = %s
+            ORDER BY id DESC LIMIT 1
+            """,
+            (id,),
+            fetch="one",
+        )
