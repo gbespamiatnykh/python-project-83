633b25928839e94fc228d3fdb80dadacd4639242
George Bespamiatnykh <george.bespamiatnykh@gmail.com>
2026-03-15 00:54:33 +0700

    feat: update url_utils.py

--- page_analyzer/url_utils.py
+++ page_analyzer/url_utils.py
@@ -3,6 +3,8 @@ from urllib.parse import urlparse
 import validators
 from bs4 import BeautifulSoup
 
+MAX_LENGTH = 200
+
 
 def normalize_url(url):
     parsed_url = urlparse(url)
@@ -18,10 +20,20 @@ def validate(url):
     return errors
 
 
+def truncate(text):
+    if not text:
+        return None
+    return text[: MAX_LENGTH - 3] + "..." if len(text) > MAX_LENGTH else text
+
+
 def parse_html(html):
     parsed_html = BeautifulSoup(html, "html.parser")
     heading = parsed_html.h1.string if parsed_html.h1 else None
     title = parsed_html.title.string if parsed_html.title else None
     description = parsed_html.find("meta", {"name": "description"})
     description_content = description.get("content") if description else None
-    return {"h1": heading, "title": title, "description": description_content}
+    return {
+        "h1": truncate(heading),
+        "title": truncate(title),
+        "description": truncate(description_content),
+    }
